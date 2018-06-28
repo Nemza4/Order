440b16747c657620236de1e7ce53d088d97ee0bb
nemza4 <nemavholags@gmail.com>
2018-06-28 15:55:50 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,49 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // value =1;
+valueone: number;
+name:string;
+whippedcream:boolean;
+quantity:string;
+chocolate:boolean;
+display:number;
+chocolatemsg:string;
+whippedcreammsg:string;
 
   constructor(public navCtrl: NavController) {
+    this.valueone =0;
+  }
+  onClick1(){
+    // console.log(this.value)
+    this.valueone -=1;
+    this.display = 0;
+    
+  }
+  onClick2(){
+    // console.log(this.value)
+    this.valueone +=1;
+   console.log(this.name);
+  }
+  onClick3(){
+    this.display=1;
 
   }
-
+  event1(){
+    if(this.whippedcream){
+     this.whippedcreammsg="+whippedcream" ;
+    }
+    
+   } 
+  event(){
+    if(this.chocolate){
+      this.chocolatemsg="+chocolate" ;
+    }
+  }
+  onClick4(){
+ this.valueone = 0;
+ this.chocolate = false;
+ this.whippedcream = false;
+ this.name = "";
+}
 }
